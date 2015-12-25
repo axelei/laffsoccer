4205d5fe2846d2739d6a837612ede29d6bd6f670
Massimo Modica <massimo@ibuildings.it>
2015-12-25 21:28:23 +0100

    Created libgdx project

---
+++ java/desktop/src/com/ygames/ysoccer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ygames.ysoccer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ygames.ysoccer.YSoccer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new YSoccer(), config);
+	}
+}
