d1482c485e2a5fb9b25e15122f1d4cd61f3b3cb7
Massimo Modica <massimo@ibuildings.it>
2015-12-26 01:15:23 +0100

    Set desktop initial screen resolution

--- java/desktop/src/com/ygames/ysoccer/desktop/DesktopLauncher.java
+++ java/desktop/src/com/ygames/ysoccer/desktop/DesktopLauncher.java
@@ -5,8 +5,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.ygames.ysoccer.YSoccer;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new YSoccer(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 1280;
+        config.height = 720;
+        new LwjglApplication(new YSoccer(), config);
+    }
 }
