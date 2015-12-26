21b9c807f58325a7bf0dfbf3565e464347b5e607
Massimo Modica <massimo@ibuildings.it>
2015-12-26 15:25:22 +0100

    Added Orthographic Camera

--- java/core/src/com/ygames/ysoccer/YSoccer.java
+++ java/core/src/com/ygames/ysoccer/YSoccer.java
@@ -1,27 +1,35 @@
 package com.ygames.ysoccer;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class YSoccer extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    private OrthographicCamera camera;
+    SpriteBatch batch;
+    Texture img;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    @Override
+    public void create() {
+        camera = new OrthographicCamera();
+        camera.setToOrtho(true, 1280, 720);
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void render() {
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        img.dispose();
+        batch.dispose();
+    }
 }
