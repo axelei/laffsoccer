44b6788096ba56d010817fd93f35e115fc137b3f
Massimo Modica <massimo.modica@tin.it>
2015-12-30 19:46:44 +0100

    Added Keyboard class

--- java/core/src/com/ygames/ysoccer/YSoccer.java
+++ java/core/src/com/ygames/ysoccer/YSoccer.java
@@ -1,13 +1,16 @@
 package com.ygames.ysoccer;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Input;
 import com.ygames.ysoccer.framework.GlGraphics;
+import com.ygames.ysoccer.framework.Keyboard;
 import com.ygames.ysoccer.framework.Settings;
 
 public class YSoccer extends Game {
 
     public Settings settings;
     public GlGraphics glGraphics;
+    Keyboard keyboard;
 
     @Override
     public void create() {
@@ -15,10 +18,13 @@ public class YSoccer extends Game {
         glGraphics = new GlGraphics();
         Assets.load();
         this.setScreen(new MenuMain(this));
+        keyboard = new Keyboard();
+        keyboard.setKeys(Input.Keys.A, Input.Keys.D, Input.Keys.W, Input.Keys.S, Input.Keys.COMMA, Input.Keys.PERIOD);
     }
 
     @Override
     public void render() {
+        keyboard.read();
         super.render();
     }
 
---
+++ java/core/src/com/ygames/ysoccer/framework/Keyboard.java
@@ -0,0 +1,28 @@
+package com.ygames.ysoccer.framework;
+
+import com.badlogic.gdx.Gdx;
+
+public class Keyboard {
+
+    private int keyLeft, keyRight, keyUp, keyDown;
+    private int button1, button2;
+
+    public int x0, y0;
+    public boolean fire10, fire20;
+
+    public void read() {
+        x0 = (Gdx.input.isKeyPressed(keyLeft) ? -1 : 0) + (Gdx.input.isKeyPressed(keyRight) ? 1 : 0);
+        y0 = (Gdx.input.isKeyPressed(keyUp) ? -1 : 0) + (Gdx.input.isKeyPressed(keyDown) ? 1 : 0);
+        fire10 = Gdx.input.isKeyPressed(button1);
+        fire20 = Gdx.input.isKeyPressed(button2);
+    }
+
+    public void setKeys(int keyLeft, int keyRight, int keyUp, int keyDown, int button1, int button2) {
+        this.keyLeft = keyLeft;
+        this.keyRight = keyRight;
+        this.keyUp = keyUp;
+        this.keyDown = keyDown;
+        this.button1 = button1;
+        this.button2 = button2;
+    }
+}
