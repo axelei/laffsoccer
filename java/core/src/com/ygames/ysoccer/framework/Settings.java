0b769d07e279aa623c74dca0ccad7d900b7d4c8e
Massimo Modica <massimo.modica@tin.it>
2015-12-31 14:52:49 +0100

    Added preferences field in Settings class

--- java/core/src/com/ygames/ysoccer/framework/Settings.java
+++ java/core/src/com/ygames/ysoccer/framework/Settings.java
@@ -1,6 +1,21 @@
 package com.ygames.ysoccer.framework;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+
 public class Settings {
+    public final String APP_NAME = "YSoccer";
+    public final int VERSION = 16;
     public final int GUI_WIDTH = 1280;
     public final int GUI_HEIGHT = 720;
+
+    Preferences preferences;
+
+    public Settings() {
+        preferences = Gdx.app.getPreferences(APP_NAME + VERSION);
+    }
+
+    public void save() {
+        preferences.flush();
+    }
 }
