bed2af2a789fe1d220ce9279c077fe75eef0dd61
Massimo Modica <massimo.modica@tin.it>
2015-12-31 15:10:16 +0100

    Added Assets.locales field

--- java/core/src/com/ygames/ysoccer/framework/Assets.java
+++ java/core/src/com/ygames/ysoccer/framework/Assets.java
@@ -1,19 +1,37 @@
 package com.ygames.ysoccer.framework;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.I18NBundle;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class Assets {
 
     public static I18NBundle strings;
+    public static List<String> locales;
     public static Font font14;
 
     public static void load(Settings settings) {
+        loadLocales();
         loadStrings(settings);
         font14 = new Font(14);
         font14.load();
+    }
+
+    private static void loadLocales() {
+        locales = new ArrayList<String>();
+        FileHandle[] files = Gdx.files.local("i18n/").list(".properties");
+        for (FileHandle file : files) {
+            String[] parts = file.nameWithoutExtension().split("_");
+            if (parts.length > 1) {
+                locales.add(parts[1]);
+            } else {
+                locales.add("en");
+            }
+        }
     }
 
     public static void loadStrings(Settings settings) {
