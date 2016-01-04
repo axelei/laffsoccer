3db7b77dd6b6a82ffa5ad6021c40770410c86d66
Massimo Modica <massimo.modica@tin.it>
2016-01-04 16:07:54 +0100

    Added DiyLeague.TitleButton class

--- java/core/src/com/ygames/ysoccer/screens/DiyLeague.java
+++ java/core/src/com/ygames/ysoccer/screens/DiyLeague.java
@@ -1,8 +1,12 @@
 package com.ygames.ysoccer.screens;
 
+import com.ygames.ysoccer.framework.Assets;
+import com.ygames.ysoccer.framework.Font;
 import com.ygames.ysoccer.framework.GlGame;
 import com.ygames.ysoccer.framework.GlScreen;
 import com.ygames.ysoccer.framework.Image;
+import com.ygames.ysoccer.gui.Button;
+import com.ygames.ysoccer.gui.Widget;
 
 public class DiyLeague extends GlScreen {
 
@@ -10,5 +14,20 @@ public class DiyLeague extends GlScreen {
         super(game);
 
         background = new Image("images/backgrounds/menu_competition.jpg");
+
+        Widget w;
+
+        w = new TitleButton();
+        widgets.add(w);
+    }
+
+    class TitleButton extends Button {
+
+        public TitleButton() {
+            setGeometry((game.settings.GUI_WIDTH - 520) / 2, 30, 520, 40);
+            setColors(0x415600, 0x5E7D00, 0x243000);
+            setText(Assets.strings.get("DESIGN DIY LEAGUE"), Font.Align.CENTER, Assets.font14);
+            setActive(false);
+        }
     }
 }
