b5b64858b6838a9c5e7232bdd55649805bc68c60
Massimo Modica <massimo.modica@tin.it>
2016-01-04 15:51:50 +0100

    Added Competition class

--- java/core/src/com/ygames/ysoccer/competitions/League.java
+++ java/core/src/com/ygames/ysoccer/competitions/League.java
@@ -1,8 +1,4 @@
 package com.ygames.ysoccer.competitions;
 
-import com.ygames.ysoccer.match.Team;
-
-public class League {
-    public String name;
-    public Team[] teams;
+public class League extends Competition {
 }
