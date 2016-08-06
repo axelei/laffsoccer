b090799c4e8d91ce98ac165bb43d26ff177981c2
Massimo Modica <massimo.modica@tin.it>
2016-08-06 17:34:25 +0200

    Added ViewStatistics.HighestScorerButton class

--- java/core/src/com/ygames/ysoccer/screens/ViewStatistics.java
+++ java/core/src/com/ygames/ysoccer/screens/ViewStatistics.java
@@ -17,6 +17,11 @@ public class ViewStatistics extends GlScreen {
         Widget w;
         w = new TitleBar();
         widgets.add(w);
+
+        w = new HighestScorerButton();
+        widgets.add(w);
+
+        selectedWidget = w;
     }
 
     public class TitleBar extends Button {
@@ -26,6 +31,20 @@ public class ViewStatistics extends GlScreen {
             setColors(0x415600, 0x5E7D00, 0x243000);
             setText(Assets.strings.get("STATISTICS"), Font.Align.CENTER, Assets.font14);
             setActive(false);
+        }
+    }
+
+    public class HighestScorerButton extends Button {
+
+        public HighestScorerButton() {
+            setGeometry((game.settings.GUI_WIDTH - 340) / 2, 300, 340, 40);
+            setColors(0x568200, 0x77B400, 0x243E00);
+            setText(Assets.strings.get("HIGHEST SCORER LIST"), Font.Align.CENTER, Assets.font14);
+        }
+
+        @Override
+        public void onFire1Down() {
+            // TODO
         }
     }
 }
