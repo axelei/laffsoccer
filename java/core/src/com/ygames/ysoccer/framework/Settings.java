d0689bb968e83ddc927488f5f174f8e118541329
Massimo Modica <massimo.modica@tin.it>
2016-08-14 19:35:25 +0200

    Added EditPlayers.PlayerNameButton class

--- java/core/src/com/ygames/ysoccer/framework/Settings.java
+++ java/core/src/com/ygames/ysoccer/framework/Settings.java
@@ -9,16 +9,19 @@ public class Settings {
     public final int GUI_WIDTH = 1280;
     public final int GUI_HEIGHT = 720;
     public String locale;
+    public int benchSize;
 
     Preferences preferences;
 
     public Settings() {
         preferences = Gdx.app.getPreferences(APP_NAME + VERSION);
         this.locale = preferences.getString("locale", "en");
+        this.benchSize = preferences.getInteger("benchSize", 5);
     }
 
     public void save() {
         preferences.putString("locale", locale);
+        preferences.putInteger("benchSize", benchSize);
         preferences.flush();
     }
 }
--- java/core/src/com/ygames/ysoccer/screens/EditPlayers.java
+++ java/core/src/com/ygames/ysoccer/screens/EditPlayers.java
@@ -6,21 +6,35 @@ import com.ygames.ysoccer.framework.GlGame;
 import com.ygames.ysoccer.framework.GlScreen;
 import com.ygames.ysoccer.gui.InputButton;
 import com.ygames.ysoccer.gui.Widget;
+import com.ygames.ysoccer.match.Const;
+import com.ygames.ysoccer.match.Player;
 import com.ygames.ysoccer.match.Team;
 
 public class EditPlayers extends GlScreen {
 
     Team team;
+    int selectedPly;
     boolean modified;
+
+    Widget[] nameButtons = new Widget[Const.FULL_TEAM];
 
     public EditPlayers(GlGame game, Team team, Boolean modified) {
         super(game);
         this.team = team;
+        selectedPly = -1;
         this.modified = modified;
 
         background = game.stateBackground;
 
         Widget w;
+
+        // players
+        for (int p = 0; p < Const.FULL_TEAM; p++) {
+            w = new PlayerNameButton(p);
+            nameButtons[p] = w;
+            updateNameButton(p);
+            widgets.add(w);
+        }
 
         w = new TeamNameButton();
         widgets.add(w);
@@ -28,10 +42,45 @@ public class EditPlayers extends GlScreen {
         selectedWidget = w;
     }
 
+    void setModified() {
+        modified = true;
+    }
+
+    class PlayerNameButton extends InputButton {
+
+        Player player;
+
+        public PlayerNameButton(int p) {
+            player = team.playerAtPosition(p);
+            setGeometry(304, 86 + 18 * p, 388, 17);
+            setText("", Font.Align.LEFT, Assets.font10);
+            setEntryLimit(28);
+        }
+
+        @Override
+        public void onUpdate() {
+            if (player != null && !player.name.equals(text)) {
+                player.name = text;
+                setModified();
+            }
+        }
+    }
+
+    void updateNameButton(int p) {
+        setPlayerWidgetColor(nameButtons[p], p);
+        if (p < team.players.size()) {
+            Player player = team.playerAtPosition(p);
+            nameButtons[p].setText(player.name);
+        } else {
+            nameButtons[p].setText("");
+        }
+        nameButtons[p].setActive(p < team.players.size());
+    }
+
     class TeamNameButton extends InputButton {
 
         public TeamNameButton() {
-            setGeometry(60, 30, 520, 40);
+            setGeometry(188, 30, 520, 40);
             setColors(0x9C522A, 0xBB5A25, 0x69381D);
             setText(team.name, Font.Align.CENTER, Assets.font14);
             setEntryLimit(16);
@@ -39,12 +88,42 @@ public class EditPlayers extends GlScreen {
 
         @Override
         public void onUpdate() {
-            team.name = text;
-            setModified();
+            if (!team.name.equals(text)) {
+                team.name = text;
+                setModified();
+            }
         }
     }
 
-    void setModified() {
-        modified = true;
+    void setPlayerWidgetColor(Widget b, int ply) {
+        // goalkeeper
+        if (ply == 0) {
+            b.setColors(0x4AC058, 0x81D38B, 0x308C3B);
+        }
+
+        // other players
+        else if (ply < 11) {
+            b.setColors(0x308C3B, 0x4AC058, 0x1F5926);
+        }
+
+        // bench
+        else if (ply < Const.TEAM_SIZE + game.settings.benchSize) {
+            b.setColors(0x2C7231, 0x40984A, 0x19431C);
+        }
+
+        // reserve
+        else if (ply < team.players.size()) {
+            b.setColors(0x404040, 0x606060, 0x202020);
+        }
+
+        // void
+        else {
+            b.setColors(0x202020, 0x404040, 0x101010);
+        }
+
+        // selected
+        if (selectedPly == ply) {
+            b.setColors(0x993333, 0xC24242, 0x5A1E1E);
+        }
     }
 }
