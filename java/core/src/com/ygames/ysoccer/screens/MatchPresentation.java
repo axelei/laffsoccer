c3742813a7f0803c1a89412d6e5dad7ace9e7f1e
Massimo Modica <massimo.modica@tin.it>
2016-10-02 17:25:51 +0200

    Added MatchPresentation class

---
+++ java/core/src/com/ygames/ysoccer/screens/MatchPresentation.java
@@ -0,0 +1,48 @@
+package com.ygames.ysoccer.screens;
+
+import com.badlogic.gdx.files.FileHandle;
+import com.ygames.ysoccer.competitions.Competition;
+import com.ygames.ysoccer.competitions.League;
+import com.ygames.ysoccer.framework.Assets;
+import com.ygames.ysoccer.framework.Font;
+import com.ygames.ysoccer.framework.GlGame;
+import com.ygames.ysoccer.framework.GlScreen;
+import com.ygames.ysoccer.framework.Image;
+import com.ygames.ysoccer.gui.Button;
+import com.ygames.ysoccer.gui.Widget;
+import com.ygames.ysoccer.match.Team;
+
+public class MatchPresentation extends GlScreen {
+
+    private FileHandle fileHandle;
+    private League league;
+    private Competition competition;
+    private Team homeTeam;
+    private Team awayTeam;
+
+    public MatchPresentation(GlGame game, FileHandle fileHandle, League league, Competition competition, Team homeTeam, Team awayTeam) {
+        super(game);
+
+        this.fileHandle = fileHandle;
+        this.league = league;
+        this.competition = competition;
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+
+        background = new Image("images/backgrounds/menu_match_presentation.jpg");
+
+        Widget w;
+        w = new TitleButton();
+        widgets.add(w);
+    }
+
+    class TitleButton extends Button {
+
+        public TitleButton() {
+            setGeometry((game.settings.GUI_WIDTH - 840) / 2, 30, 840, 44);
+            setColors(0x008080, 0x00B2B4, 0x004040);
+            setText(competition.name, Font.Align.CENTER, Assets.font14);
+            setActive(false);
+        }
+    }
+}
