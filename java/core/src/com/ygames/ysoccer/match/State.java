69e7879b5cf1ea69eea647d6605dc95bdc06eddf
Massimo Modica <massimo.modica@tin.it>
2016-10-20 18:38:59 +0200

    Added State class

---
+++ java/core/src/com/ygames/ysoccer/match/State.java
@@ -0,0 +1,35 @@
+package com.ygames.ysoccer.match;
+
+class State {
+
+    private int timer;
+    int id;
+
+    int getId() {
+        return id;
+    }
+
+    void doActions() {
+        timer += 1;
+    }
+
+    State checkConditions() {
+        return null;
+    }
+
+    void entryActions() {
+        timer = 0;
+    }
+
+    void exitActions() {
+    }
+
+    boolean checkId(int id) {
+        return (this.id == id);
+    }
+
+    @Override
+    public String toString() {
+        return id + ":" + timer;
+    }
+}
