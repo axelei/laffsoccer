d4f6f7e6b61579d3341b2f4b7b33c7164c8d0559
Massimo Modica <massimo.modica@tin.it>
2016-10-20 22:08:20 +0200

    Added PlayerState class

---
+++ java/core/src/com/ygames/ysoccer/match/PlayerState.java
@@ -0,0 +1,10 @@
+package com.ygames.ysoccer.match;
+
+class PlayerState extends State {
+
+    protected final Player player;
+
+    public PlayerState(Player player) {
+        this.player = player;
+    }
+}
