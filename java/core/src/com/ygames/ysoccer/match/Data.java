c82932f2e1285bcaa415051dcd6ce4c9d675667f
Massimo Modica <massimo.modica@tin.it>
2016-10-21 13:22:20 +0200

    Added Data class

---
+++ java/core/src/com/ygames/ysoccer/match/Data.java
@@ -0,0 +1,10 @@
+package com.ygames.ysoccer.match;
+
+class Data {
+    public int x;
+    public int y;
+    public int z;
+    public int fmx;
+    public int fmy;
+    public boolean isVisible;
+}
