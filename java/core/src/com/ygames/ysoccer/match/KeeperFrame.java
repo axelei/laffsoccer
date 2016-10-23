56a6f0710ee88bffcd68b3749490856b2cf44505
Massimo Modica <massimo.modica@tin.it>
2016-10-23 22:29:37 +0200

    Added KeeperFrame class

---
+++ java/core/src/com/ygames/ysoccer/match/KeeperFrame.java
@@ -0,0 +1,8 @@
+package com.ygames.ysoccer.match;
+
+class KeeperFrame {
+    int[] fmx; // top-right, top-left, bottom-right, bottom-left
+    int fmy;
+    int[] offx; // top-right, top-left, bottom-right, bottom-left
+    int offz;
+}
