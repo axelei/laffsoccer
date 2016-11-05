efe214e8804e45807ca664e7fe787390f6657d89
Massimo Modica <massimo.modica@tin.it>
2016-11-05 23:53:09 +0100

    ControlsSetup.ExitButton class

--- java/core/src/com/ygames/ysoccer/screens/ControlsSetup.java
+++ java/core/src/com/ygames/ysoccer/screens/ControlsSetup.java
@@ -17,6 +17,11 @@ class ControlsSetup extends GLScreen {
         Widget w;
         w = new TitleButton();
         widgets.add(w);
+
+        w = new ExitButton();
+        widgets.add(w);
+
+        selectedWidget = w;
     }
 
     private class TitleButton extends Button {
@@ -26,6 +31,21 @@ class ControlsSetup extends GLScreen {
             setGeometry((game.settings.GUI_WIDTH - 400) / 2, 20, 400, 40);
             setText(Assets.strings.get("CONTROLS"), Font.Align.CENTER, Assets.font14);
             setActive(false);
+        }
+    }
+
+    private class ExitButton extends Button {
+
+        ExitButton() {
+            setColors(0xC84200);
+            setGeometry((game.settings.GUI_WIDTH - 180) / 2, 660, 180, 36);
+            setText(Assets.strings.get("EXIT"), Font.Align.CENTER, Assets.font14);
+        }
+
+        @Override
+        public void onFire1Down() {
+            game.settings.save();
+            game.setScreen(new Main(game));
         }
     }
 }
