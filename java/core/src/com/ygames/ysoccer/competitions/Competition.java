76c1ba935172a3cc443f0a2fbbca6cba86b9d0ca
Massimo Modica <massimo.modica@tin.it>
2016-11-21 21:02:21 +0100

    Fix: competition extension

--- java/core/src/com/ygames/ysoccer/competitions/Competition.java
+++ java/core/src/com/ygames/ysoccer/competitions/Competition.java
@@ -142,7 +142,7 @@ public abstract class Competition {
     }
 
     public void save() {
-        FileHandle fileHandle = Assets.savesFolder.child(getCategoryFolder()).child(filename + ".JSON");
+        FileHandle fileHandle = Assets.savesFolder.child(getCategoryFolder()).child(filename + ".json");
         fileHandle.writeString(Assets.json.toJson(this, Competition.class), false, "UTF-8");
     }
 
