ccf833a38e5d1e6c051649f97c587f7e8324f835
Massimo Modica <massimo.modica@tin.it>
2016-12-10 13:12:40 +0100

    Fix: brighter kit colors

--- java/core/src/com/ygames/ysoccer/match/Kit.java
+++ java/core/src/com/ygames/ysoccer/match/Kit.java
@@ -78,27 +78,27 @@ public class Kit implements Json.Serializable {
 
         // shirt1
         rgbPairs.add(new RgbPair(0xE20020, shirt1));
-        rgbPairs.add(new RgbPair(0xBF001B, GLColor.darker(shirt1, 0.8D)));
-        rgbPairs.add(new RgbPair(0x9C0016, GLColor.darker(shirt1, 0.6D)));
-        rgbPairs.add(new RgbPair(0x790011, GLColor.darker(shirt1, 0.4D)));
+        rgbPairs.add(new RgbPair(0xBF001B, GLColor.darker(shirt1, 0.9)));
+        rgbPairs.add(new RgbPair(0x9C0016, GLColor.darker(shirt1, 0.8)));
+        rgbPairs.add(new RgbPair(0x790011, GLColor.darker(shirt1, 0.7)));
 
         // shirt2
         rgbPairs.add(new RgbPair(0x01FFC6, shirt2));
-        rgbPairs.add(new RgbPair(0x00C79B, GLColor.darker(shirt2, 0.8D)));
-        rgbPairs.add(new RgbPair(0x008B6C, GLColor.darker(shirt2, 0.6D)));
-        rgbPairs.add(new RgbPair(0x006A52, GLColor.darker(shirt2, 0.4D)));
+        rgbPairs.add(new RgbPair(0x00C79B, GLColor.darker(shirt2, 0.9)));
+        rgbPairs.add(new RgbPair(0x008B6C, GLColor.darker(shirt2, 0.8)));
+        rgbPairs.add(new RgbPair(0x006A52, GLColor.darker(shirt2, 0.7)));
 
         // shorts
         rgbPairs.add(new RgbPair(0xF6FF00, shorts));
-        rgbPairs.add(new RgbPair(0xCDD400, GLColor.darker(shorts, 0.8D)));
-        rgbPairs.add(new RgbPair(0xA3A900, GLColor.darker(shorts, 0.6D)));
-        rgbPairs.add(new RgbPair(0x7A7E00, GLColor.darker(shorts, 0.4D)));
+        rgbPairs.add(new RgbPair(0xCDD400, GLColor.darker(shorts, 0.9)));
+        rgbPairs.add(new RgbPair(0xA3A900, GLColor.darker(shorts, 0.8)));
+        rgbPairs.add(new RgbPair(0x7A7E00, GLColor.darker(shorts, 0.7)));
 
         // socks
         rgbPairs.add(new RgbPair(0x0097EE, socks));
-        rgbPairs.add(new RgbPair(0x0088D6, GLColor.darker(socks, 0.8D)));
-        rgbPairs.add(new RgbPair(0x0079BF, GLColor.darker(socks, 0.6D)));
-        rgbPairs.add(new RgbPair(0x006AA7, GLColor.darker(socks, 0.4D)));
+        rgbPairs.add(new RgbPair(0x0088D6, GLColor.darker(socks, 0.9)));
+        rgbPairs.add(new RgbPair(0x0079BF, GLColor.darker(socks, 0.8)));
+        rgbPairs.add(new RgbPair(0x006AA7, GLColor.darker(socks, 0.7)));
     }
 
     public static float colorDifference(Kit homeKit, Kit awayKit) {
