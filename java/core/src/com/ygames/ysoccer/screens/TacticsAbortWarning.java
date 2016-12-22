f59686601d16b6cf11b0518e07b2e1d0e28e0f73
Massimo Modica <massimo.modica@tin.it>
2016-12-22 18:48:30 +0100

    TacticsAbortWarning.WarningButton class

--- java/core/src/com/ygames/ysoccer/screens/TacticsAbortWarning.java
+++ java/core/src/com/ygames/ysoccer/screens/TacticsAbortWarning.java
@@ -2,8 +2,10 @@ package com.ygames.ysoccer.screens;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.ygames.ysoccer.framework.Assets;
+import com.ygames.ysoccer.framework.Font;
 import com.ygames.ysoccer.framework.GLGame;
 import com.ygames.ysoccer.framework.GLScreen;
+import com.ygames.ysoccer.gui.Button;
 import com.ygames.ysoccer.gui.Widget;
 
 class TacticsAbortWarning extends GLScreen {
@@ -17,5 +19,18 @@ class TacticsAbortWarning extends GLScreen {
 
         w = new TitleBar(Assets.strings.get("EDIT TACTICS"), 0xBA9206);
         widgets.add(w);
+
+        w = new WarningButton();
+        widgets.add(w);
+    }
+
+    private class WarningButton extends Button {
+
+        WarningButton() {
+            setGeometry(640 - 620 / 2, 260, 620, 180);
+            setColors(0xDC0000, 0xFF4141, 0x8C0000);
+            setText(Assets.strings.get("TACTICS.EDITED TACTICS WILL BE LOST"), Font.Align.CENTER, Assets.font14);
+            setActive(false);
+        }
     }
 }
