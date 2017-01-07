e43d3f85b55221dba44fda075ba2cb641056b957
Massimo Modica <massimo.modica@tin.it>
2017-01-07 14:54:41 +0100

    Desktop application icons

--- java/desktop/src/com/ygames/ysoccer/desktop/DesktopLauncher.java
+++ java/desktop/src/com/ygames/ysoccer/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.ygames.ysoccer.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.ygames.ysoccer.YSoccer;
@@ -9,6 +10,8 @@ public class DesktopLauncher {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.width = 1280;
         config.height = 720;
+        config.addIcon("images/icon_128.png", Files.FileType.Internal);
+        config.addIcon("images/icon_32.png", Files.FileType.Internal);
         new LwjglApplication(new YSoccer(), config);
     }
 }
