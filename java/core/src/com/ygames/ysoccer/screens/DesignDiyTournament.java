0b9bb04e81cb5a98419d77b88635d25fbf1b52c9
Massimo Modica <massimo.modica@tin.it>
2018-08-07 10:02:31 +0200

    DesignDiyTournament.AbortButton class

--- java/core/src/com/ygames/ysoccer/screens/DesignDiyTournament.java
+++ java/core/src/com/ygames/ysoccer/screens/DesignDiyTournament.java
@@ -6,6 +6,7 @@ import com.ygames.ysoccer.framework.Assets;
 import com.ygames.ysoccer.framework.Font;
 import com.ygames.ysoccer.framework.GLGame;
 import com.ygames.ysoccer.framework.GLScreen;
+import com.ygames.ysoccer.gui.Button;
 import com.ygames.ysoccer.gui.InputButton;
 import com.ygames.ysoccer.gui.Widget;
 
@@ -29,6 +30,9 @@ class DesignDiyTournament extends GLScreen {
 
         w = new TournamentNameButton();
         widgets.add(w);
+
+        w = new AbortButton();
+        widgets.add(w);
     }
 
     private class TournamentNameButton extends InputButton {
@@ -43,6 +47,20 @@ class DesignDiyTournament extends GLScreen {
         @Override
         public void onChanged() {
             tournament.name = text;
+        }
+    }
+
+    private class AbortButton extends Button {
+
+        AbortButton() {
+            setColors(0xC8000E);
+            setGeometry((game.gui.WIDTH - 180) / 2, 660, 180, 36);
+            setText(Assets.strings.get("ABORT"), Font.Align.CENTER, Assets.font14);
+        }
+
+        @Override
+        public void onFire1Down() {
+            game.setScreen(new Main(game));
         }
     }
 }
