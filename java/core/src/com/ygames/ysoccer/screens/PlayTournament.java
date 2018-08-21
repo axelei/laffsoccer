eb952e152b525da06680254d62c2a93f78184f0a
Massimo Modica <massimo.modica@tin.it>
2018-08-21 13:02:11 +0200

    PlayTournament.VersusLabel class

--- java/core/src/com/ygames/ysoccer/screens/PlayTournament.java
+++ java/core/src/com/ygames/ysoccer/screens/PlayTournament.java
@@ -7,6 +7,7 @@ import com.ygames.ysoccer.framework.Font;
 import com.ygames.ysoccer.framework.GLGame;
 import com.ygames.ysoccer.framework.GLScreen;
 import com.ygames.ysoccer.gui.Button;
+import com.ygames.ysoccer.gui.Label;
 import com.ygames.ysoccer.gui.Widget;
 import com.ygames.ysoccer.match.Match;
 import com.ygames.ysoccer.match.Team;
@@ -64,6 +65,10 @@ class PlayTournament extends GLScreen {
                     resultWidgets.add(w);
                     widgets.add(w);
 
+                    w = new VersusLabel(dy + 64 * m, match);
+                    resultWidgets.add(w);
+                    widgets.add(w);
+
                     w = new TeamButton(705, dy + 64 * m, tournament.teams.get(match.teams[AWAY]), Font.Align.LEFT, qualified == match.teams[AWAY]);
                     resultWidgets.add(w);
                     widgets.add(w);
@@ -99,6 +104,19 @@ class PlayTournament extends GLScreen {
             int borderColor = qualified ? 0x21E337 : 0x1A1A1A;
             setColors(bodyColor, borderColor, borderColor);
             setText(team.name, align, Assets.font10);
+            setActive(false);
+        }
+    }
+
+    private class VersusLabel extends Label {
+
+        VersusLabel(int y, Match match) {
+            setGeometry((game.gui.WIDTH - 30) / 2, y, 30, 26);
+            // NOTE: max 2 characters
+            setText(Assets.strings.get("ABBREVIATIONS.VERSUS"), Font.Align.CENTER, Assets.font10);
+            if (match.isEnded()) {
+                setText("-");
+            }
             setActive(false);
         }
     }
