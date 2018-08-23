a42296104e77e2ec41a95bbb8e96490e38d1212e
Massimo Modica <massimo.modica@tin.it>
2018-08-23 19:01:40 +0200

    Group.calendar field

--- java/core/src/com/ygames/ysoccer/competitions/tournament/groups/Group.java
+++ java/core/src/com/ygames/ysoccer/competitions/tournament/groups/Group.java
@@ -2,10 +2,19 @@ package com.ygames.ysoccer.competitions.tournament.groups;
 
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonValue;
+import com.ygames.ysoccer.match.Match;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Group implements Json.Serializable {
 
     private Groups groups;
+    public ArrayList<Match> calendar;
+
+    Group() {
+        calendar = new ArrayList<Match>();
+    }
 
     public void setGroups(Groups groups) {
         this.groups = groups;
@@ -13,9 +22,12 @@ public class Group implements Json.Serializable {
 
     @Override
     public void read(Json json, JsonValue jsonData) {
+        Match[] calendarArray = json.readValue("calendar", Match[].class, jsonData);
+        Collections.addAll(calendar, calendarArray);
     }
 
     @Override
     public void write(Json json) {
+        json.writeValue("calendar", calendar, Match[].class, Match.class);
     }
 }
