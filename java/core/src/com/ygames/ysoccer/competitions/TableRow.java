05806b35d183eff78c4615ab9fddb8a707a2825e
Massimo Modica <massimo.modica@tin.it>
2018-08-23 19:26:37 +0200

    TableRow class

---
+++ java/core/src/com/ygames/ysoccer/competitions/TableRow.java
@@ -0,0 +1,42 @@
+package com.ygames.ysoccer.competitions;
+
+public class TableRow {
+
+    public int team;
+
+    public int won;
+    public int drawn;
+    public int lost;
+
+    public int goalsFor;
+    public int goalsAgainst;
+    public int points;
+
+    TableRow(int team) {
+        this.team = team;
+    }
+
+    public void update(int goalsFor, int goalsAgainst, int pointsForAWin) {
+        this.goalsFor += goalsFor;
+        this.goalsAgainst += goalsAgainst;
+        if (goalsFor > goalsAgainst) {
+            won += 1;
+            points += pointsForAWin;
+        } else if (goalsFor == goalsAgainst) {
+            drawn += 1;
+            points += 1;
+        } else {
+            lost += 1;
+        }
+    }
+
+    void reset() {
+        won = 0;
+        drawn = 0;
+        lost = 0;
+
+        goalsFor = 0;
+        goalsAgainst = 0;
+        points = 0;
+    }
+}
