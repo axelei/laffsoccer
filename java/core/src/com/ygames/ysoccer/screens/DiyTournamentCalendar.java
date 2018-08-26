d05c125025c22180a1a70fd9743ce98c9a0a1038
Massimo Modica <massimo.modica@tin.it>
2018-08-26 10:37:15 +0200

    DiyTournamentCalendar.Mode enum

--- java/core/src/com/ygames/ysoccer/screens/DiyTournamentCalendar.java
+++ java/core/src/com/ygames/ysoccer/screens/DiyTournamentCalendar.java
@@ -1,6 +1,7 @@
 package com.ygames.ysoccer.screens;
 
 import com.ygames.ysoccer.competitions.tournament.Tournament;
+import com.ygames.ysoccer.competitions.tournament.groups.Groups;
 import com.ygames.ysoccer.framework.GLGame;
 import com.ygames.ysoccer.framework.GLScreen;
 import com.ygames.ysoccer.gui.Widget;
@@ -8,6 +9,10 @@ import com.ygames.ysoccer.gui.Widget;
 class DiyTournamentCalendar extends GLScreen {
 
     private Tournament tournament;
+
+    private enum Mode {GROUPS_DISTRIBUTION, GROUP_MATCHES, KNOCKOUT_CALENDAR}
+
+    private Mode mode;
 
     DiyTournamentCalendar(GLGame game, Tournament tournament) {
         super(game);
@@ -18,5 +23,12 @@ class DiyTournamentCalendar extends GLScreen {
 
         w = new TitleBar("DIY TOURNAMENT CALENDAR", game.stateColor.body);
         widgets.add(w);
+
+        switch (tournament.getRound().type) {
+            case GROUPS:
+                Groups groups = (Groups) tournament.getRound();
+                mode = Mode.GROUPS_DISTRIBUTION;
+                break;
+        }
     }
 }
