d4ac5fcb282503924921f617de3150dfd7142a6e
Massimo Modica <massimo.modica@tin.it>
2018-08-26 16:53:51 +0200

    DiyTournamentCalendar.PlayButton class

--- java/core/src/com/ygames/ysoccer/screens/DiyTournamentCalendar.java
+++ java/core/src/com/ygames/ysoccer/screens/DiyTournamentCalendar.java
@@ -1,6 +1,7 @@
 package com.ygames.ysoccer.screens;
 
 import com.ygames.ysoccer.competitions.tournament.Tournament;
+import com.ygames.ysoccer.competitions.tournament.groups.Group;
 import com.ygames.ysoccer.competitions.tournament.groups.Groups;
 import com.ygames.ysoccer.framework.Assets;
 import com.ygames.ysoccer.framework.Font;
@@ -20,11 +21,12 @@ import static com.ygames.ysoccer.match.Match.HOME;
 
 class DiyTournamentCalendar extends GLScreen {
 
+    private Tournament tournament;
     private Groups groups;
     private int currentGroup;
     private int currentMatch;
     private int matchSide;
-    private int groupsTeams;
+    private List<Integer> groupsTeams;
     private List<Widget> teamButtons;
 
     private enum Mode {GROUPS_DISTRIBUTION, GROUP_MATCHES, KNOCKOUT_CALENDAR}
@@ -33,6 +35,8 @@ class DiyTournamentCalendar extends GLScreen {
 
     DiyTournamentCalendar(GLGame game, Tournament tournament) {
         super(game);
+        this.tournament = tournament;
+        groupsTeams = new ArrayList<Integer>();
 
         background = game.stateBackground;
         Widget w;
@@ -62,6 +66,9 @@ class DiyTournamentCalendar extends GLScreen {
             Widget.arrange(game.gui.WIDTH, 392, 29, teamButtons);
             setSelectedWidget(teamButtons.get(0));
         }
+
+        w = new PlayButton();
+        widgets.add(w);
     }
 
     private class TeamButton extends Button {
@@ -123,12 +130,12 @@ class DiyTournamentCalendar extends GLScreen {
         public void onFire1Down() {
             switch (mode) {
                 case GROUPS_DISTRIBUTION:
-                    groupIndex = groupsTeams / groups.groupNumberOfTeams();
-                    groupsTeams++;
+                    groupIndex = groupsTeams.size() / groups.groupNumberOfTeams();
+                    groupsTeams.add(teamIndex);
                     done = true;
 
                     // advance to GROUP_MATCHES mode
-                    if (groupsTeams == game.teamList.size()) {
+                    if (groupsTeams.size() == game.teamList.size()) {
                         for (Widget w : teamButtons) {
                             ((TeamButton) w).done = false;
                         }
@@ -157,6 +164,63 @@ class DiyTournamentCalendar extends GLScreen {
                     }
             }
             refreshAllWidgets();
+        }
+    }
+
+    private class PlayButton extends Button {
+
+        PlayButton() {
+            setGeometry(game.gui.WIDTH / 2 + 110, 660, 360, 36);
+            setText("PLAY TOURNAMENT", Font.Align.CENTER, Assets.font14);
+        }
+
+        @Override
+        public void refresh() {
+            switch (mode) {
+                case GROUP_MATCHES:
+                    if (currentGroup == groups.groups.size() && currentMatch == 0) {
+                        setColors(0x138B21);
+                        setActive(true);
+                    } else {
+                        setColors(0x666666);
+                        setActive(false);
+                    }
+                    break;
+            }
+        }
+
+        @Override
+        public void onFire1Down() {
+            switch (mode) {
+                case GROUP_MATCHES:
+                    for (int g = 0; g < groups.groups.size(); g++) {
+                        Group group = groups.groups.get(g);
+                        // add other rounds
+                        for (int r = 1; r < groups.rounds; r++) {
+                            for (int i = 0; i < (groups.groupNumberOfTeams() * (groups.groupNumberOfTeams() - 1) / 2); i++) {
+                                Match firstRoundMatch = group.calendar.get(i);
+                                Match match = new Match();
+                                if (r % 2 == 0) {
+                                    match.teams[HOME] = firstRoundMatch.teams[HOME];
+                                    match.teams[AWAY] = firstRoundMatch.teams[AWAY];
+                                } else {
+                                    match.teams[HOME] = firstRoundMatch.teams[AWAY];
+                                    match.teams[AWAY] = firstRoundMatch.teams[HOME];
+                                }
+                                group.calendar.add(match);
+                            }
+                        }
+                        // populate table
+                        ArrayList<Integer> teams = new ArrayList<Integer>();
+                        for (int t = 0; t < groups.groupNumberOfTeams(); t++) {
+                            teams.add(groupsTeams.get(g * groups.groupNumberOfTeams() + t));
+                        }
+                        group.populateTable(teams);
+                    }
+                    tournament.start(game.teamList);
+                    game.setCompetition(tournament);
+                    game.setScreen(new PlayTournament(game));
+            }
         }
     }
 }
