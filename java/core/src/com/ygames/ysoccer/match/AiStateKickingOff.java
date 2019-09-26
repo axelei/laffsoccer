4187ae0b64e757b8bf64ffe68da24d226d4acde4
Massimo Modica <massimo.modica@tin.it>
2019-09-26 09:50:08 +0200

    Ai kick off calibration

--- java/core/src/com/ygames/ysoccer/match/AiStateKickingOff.java
+++ java/core/src/com/ygames/ysoccer/match/AiStateKickingOff.java
@@ -1,6 +1,5 @@
 package com.ygames.ysoccer.match;
 
-import com.ygames.ysoccer.framework.GLGame;
 import com.ygames.ysoccer.math.Emath;
 
 import static com.ygames.ysoccer.match.AiFsm.Id.STATE_KICKING_OFF;
@@ -15,12 +14,12 @@ class AiStateKickingOff extends AiState {
     void doActions() {
         super.doActions();
         ai.x0 = player.team.side;
-        ai.fire10 = Emath.isIn(timer, 1.0f * GLGame.VIRTUAL_REFRESH_RATE, 1.05f * GLGame.VIRTUAL_REFRESH_RATE);
+        ai.fire10 = Emath.isIn(timer, 50, 56);
     }
 
     @Override
     State checkConditions() {
-        if (timer > 1.2f * GLGame.VIRTUAL_REFRESH_RATE) {
+        if (timer > 70) {
             return fsm.stateIdle;
         }
         return null;
