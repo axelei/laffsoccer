7eabfc222be154c760b9c00e2bb6875dbf8e76ed
Krusher <axelei@gmail.com>
2020-01-02 00:51:58 +0100

    ...mejor con JavaFX.

---
+++ java/desktop/src/com/ygames/ysoccer/desktop/HelloFX.java
@@ -0,0 +1,29 @@
+package com.ygames.ysoccer.desktop;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+import javafx.scene.media.MediaPlayer;
+
+public class HelloFX extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
