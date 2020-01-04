e6fc9beecd6a9cd340d75d35e9650cbb67dae4c5
Krusher <axelei@gmail.com>
2020-01-04 15:29:22 +0100

    Rama para utilidades

---
+++ java/utils/src/net/krusher/laffsoccer/util/Auxiliary.java
@@ -0,0 +1,4 @@
+package net.krusher.laffsoccer.util;
+
+public class Auxiliary {
+}
---
+++ java/utils/src/net/krusher/laffsoccer/util/GenerateTeam.java
@@ -0,0 +1,12 @@
+import com.ygames.ysoccer.match.Team;
+
+public class GenerateTeam {
+
+    public static void main(String[] arg) {
+
+        System.out.println("Laffsoccer utilities");
+
+        Team team = new Team();
+
+    }
+}
