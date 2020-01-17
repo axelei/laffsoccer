09980a9537b4435a86dadde7cffc1011248ed3a3
Krusher <axelei@gmail.com>
2020-01-17 20:54:47 +0100

    De momento de momento un saco de cemento

--- java/core/src/com/ygames/ysoccer/YSoccer.java
+++ java/core/src/com/ygames/ysoccer/YSoccer.java
@@ -2,8 +2,10 @@ package com.ygames.ysoccer;
 
 import com.ygames.ysoccer.framework.GLGame;
 import com.ygames.ysoccer.framework.MenuMusic;
+import com.ygames.ysoccer.screens.Intro;
 import com.ygames.ysoccer.screens.Main;
 import com.ygames.ysoccer.screens.Video;
+import org.apache.commons.lang3.SystemUtils;
 
 public class YSoccer extends GLGame {
 
@@ -11,12 +13,12 @@ public class YSoccer extends GLGame {
     public void create() {
         super.create();
 
-        if (settings.showIntro) {
+        if (settings.showIntro && SystemUtils.IS_OS_WINDOWS) {
             this.setScreen(new Video(this));
         } else {
             menuMusic.setMode(settings.musicMode);
             prematchMusic.setMode(MenuMusic.ALL);
-            this.setScreen(new Main(this));
+            this.setScreen(new Intro(this));
         }
     }
 }
