3e213dfd9314a0fdbb043c75b42303684fc8e37f
Krusher <axelei@gmail.com>
2021-03-07 16:08:06 +0100

    Intento infructuoso de video

--- java/core/src/com/ygames/ysoccer/YSoccer.java
+++ java/core/src/com/ygames/ysoccer/YSoccer.java
@@ -1,16 +1,25 @@
 package com.ygames.ysoccer;
 
+import com.badlogic.gdx.video.VideoPlayerInitException;
 import com.ygames.ysoccer.framework.GLGame;
 import com.ygames.ysoccer.framework.MenuMusic;
 import com.ygames.ysoccer.screens.Title;
 import com.ygames.ysoccer.screens.Intro;
+import com.ygames.ysoccer.screens.Video;
 import org.apache.commons.lang3.SystemUtils;
+
+import java.io.IOException;
 
 public class YSoccer extends GLGame {
 
     @Override
     public void create() {
         super.create();
-        this.setScreen(new Intro(this));
+        //this.setScreen(new Intro(this));
+        try {
+            this.setScreen(new Video(this));
+        } catch (IOException | VideoPlayerInitException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
---
+++ java/core/src/com/ygames/ysoccer/screens/Video.java
@@ -0,0 +1,90 @@
+package com.ygames.ysoccer.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.video.VideoPlayer;
+import com.badlogic.gdx.video.VideoPlayerCreator;
+import com.badlogic.gdx.video.VideoPlayerInitException;
+import com.ygames.ysoccer.framework.GLGame;
+import com.ygames.ysoccer.framework.GLScreen;
+import com.ygames.ysoccer.framework.MenuMusic;
+import com.ygames.ysoccer.gui.TextBox;
+import com.ygames.ysoccer.gui.Widget;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Video extends GLScreen {
+
+    private VideoPlayer player = VideoPlayerCreator.createVideoPlayer();
+
+    public Video(GLGame game) throws VideoPlayerInitException, IOException {
+        super(game);
+
+        usesMouse = false;
+
+        game.disableMouse();
+        Gdx.input.setInputProcessor(new IntroInputProcessor());
+
+        FileHandle intro = Gdx.files.local("videos").child("intro2.ogg");
+
+        try {
+            player.play(intro);
+        } catch (FileNotFoundException | NoClassDefFoundError e) {
+            e.printStackTrace();
+            setMainMenu();
+        }
+
+        player.setOnCompletionListener(event -> setMainMenu());
+        player.resize(1,1);
+
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+
+        try {
+            player.render();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            setMainMenu();
+        }
+
+        if (widgetEvent == Widget.Event.FIRE1_UP || widgetEvent == Widget.Event.FIRE2_UP) {
+            setMainMenu();
+        }
+    }
+
+    private class IntroInputProcessor extends InputAdapter {
+
+        @Override
+        public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+            setMainMenu();
+            return true;
+        }
+
+        @Override
+        public boolean keyUp(int keycode) {
+            setMainMenu();
+            return true;
+        }
+    }
+
+    private void setMainMenu() {
+        if (player.isPlaying()) {
+            player.stop();
+        }
+        Gdx.input.setInputProcessor(null);
+        game.enableMouse();
+        game.menuMusic.setMode(game.settings.musicMode);
+        game.prematchMusic.setMode(MenuMusic.ALL);
+        game.setScreen(new Title(game));
+    }
+}
