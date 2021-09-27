dd493935e8035a50194e36a4a396f55ebc1bc9ef
Krusher <axelei@gmail.com>
2021-09-27 14:51:00 +0200

    Pruebas de empaquetamiento automático

---
+++ java/desktop/package.cmd
@@ -0,0 +1,13 @@
+set launch4j="C:\Program Files (x86)\Launch4j\launch4jc.exe"
+set zip="zip"
+set jdk="D:\java\corretto\jdk17.0.0_35"
+
+rmdir build\windows /q /s
+
+%jdk%\bin\jar --update --file build\libs\desktop-1.0.jar --main-class com.ygames.ysoccer.desktop.DesktopLauncher
+
+%launch4j% launch4j.xml
+
+mkdir build\windows
+copy build\libs\Charnego-Internatiolaff-Soccer.exe build\windows
+xcopy /E/H %jdk% build\windows\jre
